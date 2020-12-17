956dde44869d98957e5ccc64e9ff1470ef90556d
cbituin <inbetweenwebdesigns@gmail.com>
2020-12-16 21:10:58 -0600

    Adds skeleton functions and sets up assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 // Code your solution here!
+const printString = (myString) => {
+    console.log(myString[0]);
+   
+    if (myString.length > 1) {
+      let mySubString = myString.substring(1, myString.length);
+      printString(mySubString);
+    } else {
+      return true;
+    }
+  };
 
+  const reverseString = (str) => {
+    console.log(str);
+  };
+
+  const isPalindrome = (str) => {
+    console.log(str);
+  };
