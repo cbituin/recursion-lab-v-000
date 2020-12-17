40df016d43e39474048256a60ebb0fe15003a30e
cbituin <inbetweenwebdesigns@gmail.com>
2020-12-17 16:32:38 -0600

    Adds additional str methods and one additional arr method

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Code your solution here!
-const printString = (myString) => {
+function printString(myString) {
     console.log(myString[0]);
    
     if (myString.length > 1) {
@@ -8,12 +7,26 @@ const printString = (myString) => {
     } else {
       return true;
     }
+  }
+  function reverseString(str) {
+    return (str.length < 2) ? str : reverseString(str.substring(1)) + str[0];
   };
 
-  const reverseString = (str) => {
-    console.log(str);
+  function isPalindrome(str) {
+    let strLength = str.length;
+
+    if (strLength < 2) {
+      return true;
+    } else if (str[strLength-1] === str[0]) {
+      return isPalindrome(str.substring(1, strLength-1));
+    } else {
+      return false;
+    }
+
   };
 
-  const isPalindrome = (str) => {
-    console.log(str);
+  function addUpTo(arr, idx) {     
+    console.log(`The array: ${arr}`);
+    console.log(`The index: ${idx}`);
+    return idx ? arr[idx] + addUpTo(arr, --idx) : arr[idx];
   };
