67cc8a822319a7200ce3d0d47ccd8af392fe54ff
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2020-12-29 11:19:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function printString(myString) {
     return (str.length < 2) ? str : reverseString(str.substring(1)) + str[0];
   };
 
+<<<<<<< HEAD
   function isPalindrome(str) {
     let strLength = str.length;
 
@@ -30,3 +31,29 @@ function printString(myString) {
     console.log(`The index: ${idx}`);
     return idx ? arr[idx] + addUpTo(arr, --idx) : arr[idx];
   };
+=======
+<<<<<<< HEAD
+  const reverseString = (str) => {
+    console.log(str);
+  };
+
+  const isPalindrome = (str) => {
+    console.log(str);
+  };
+=======
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+const reverseString= (str) => {
+  
+};
+>>>>>>> 42456fd6561a62aba3b5c2d062389784c7532d26
+>>>>>>> 2cfb8ad8d1c396bc9bad1af48136af5a190462d9
